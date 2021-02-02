8e7e114e4381c5e8df4c417691d236f229e6062f
OJrogue <53382501+OJrogue@users.noreply.github.com>
2021-02-02 18:18:21 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,20 +1,41 @@
 import discord
 import os
-#import pynacl
-#import dnspython
 import server
-from discord.ext import commands
+import discord
+import functions
 
-bot = commands.Bot(command_prefix="!")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-@bot.event
+@client.event
 async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
+    print("Logged in as: {0.user}".format(client))
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    message_split = message.content.lower().split(" ")
+    if message_split[0] == "points":
+
+        name = message_split[1].replace("_"," ")
+
+
+        points = ft.read_points(name)
+
+        if points:
+            embedVar = discord.Embed(title=points[0],description=points[1],color=0x3498db)
+            embedVar.add_field(name="Points", value=points[2], inline=False)
+            embedVar.add_field(name="Armor", value=points[3], inline=True)
+            embedVar.add_field(name="Weapons", value=points[4], inline=True)
+            embedVar.add_field(name="Echoes", value=points[5], inline=True)
+
+            await message.channel.send(embed=embedVar)
+
+        else:
+            await message.channel.send("Toon not found")
+
 
 server.server()
 bot.run(TOKEN)
